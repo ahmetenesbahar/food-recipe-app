ef56353e33c352349a44454ca40b2e06a8ba382b
ahmetenesbahar <enesbahar2303@gmail.com>
2022-09-26 21:33:54 +0300

    food recipe app

---
+++ script.js
@@ -0,0 +1,42 @@
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+const APP_ID = "b54a0871";
+const APP_key = "43a41b92a2d3c9bfa2c402954d1a1d1d";
+// console.log(container)
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
+});
+
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
+
+function generateHTML(results) {
+  container.classList.remove("initial");
+  let generatedHTML = "";
+  results.map((result) => {
+    generatedHTML += `
+      <div class="item">
+        <img src="${result.recipe.image}" alt="img">
+        <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="view-button" target="_blank" href="${
+            result.recipe.url
+          }">View Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${result.recipe.calories.toFixed(
+          2
+        )} kcal</p>
+      </div>
+    `;
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+}
